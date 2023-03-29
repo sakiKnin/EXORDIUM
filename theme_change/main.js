bcf1acb92e5fe01e2dc3f5593f6b169178475776
unknown <boris.saric@gmail.com>
2023-03-29 13:59:25 +0200

    theme change

--- theme_change/main.js
+++ theme_change/main.js
@@ -2,23 +2,22 @@
 function setTheme(themeName) {
 	
     localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+    document.documentElement.id = themeName;
 }
 // function to toggle between light and dark theme
 function toggleTheme(e) {
 		let theme=e.target.id;
-		if(theme==="theme1"){
-			setTheme("theme-1");
+		 
+		if(localStorage.getItem('theme')==theme){
+			return;
 		}else{
-			setTheme("theme-2");
+			setTheme(theme);
 		}
-    
+		 	 
 }
 // Immediately invoked function to set the theme on initial load
 (function () {
-   if (localStorage.getItem('theme') === 'theme-2') {
-       setTheme('theme-2');
-   } else {
-       setTheme('theme-1');
-   }
+   
+       setTheme('theme1');
+   
 })();
